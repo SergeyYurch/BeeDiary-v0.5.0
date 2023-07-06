00c17e938836d293b769929981a5507947ccc655
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-06 23:24:31 +0300

    create dto for queen

---
+++ src/apiary/features/queens/dto/input/create-queen.dto.ts
@@ -0,0 +1,7 @@
+export class CreateQueenDto {
+  bread: string;
+  yearOfBirth: number;
+  monthOfFlyby: number;
+  condition: number;
+  note: string;
+}
---
+++ src/apiary/features/queens/dto/view/queen.view.dto.ts
@@ -0,0 +1,7 @@
+export class QueenViewDto {
+  id: string;
+  bread: string;
+  yearOfBirth: number;
+  condition: number;
+  note: number;
+}
