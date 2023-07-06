a184298a67057e27a9aa68750628be1c9c710122
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-06 23:25:33 +0300

    add colony.controller.ts

--- src/apiary/api/colony.controller.ts
+++ src/apiary/api/colony.controller.ts
@@ -7,33 +7,53 @@ import {
   Delete,
   Put,
 } from '@nestjs/common';
-import { CreateColonyDto } from '../features/colonies/dto/create-colony.dto';
-import { UpdateColonyDto } from '../features/colonies/dto/update-colony.dto';
+import { CreateColonyDto } from '../features/colonies/dto/input/create-colony.dto';
+import { UpdateColonyDto } from '../features/colonies/dto/input/update-colony.dto';
+import { BaseControllerInterface } from '../../common/interfaces/baseControllerInterface';
+import { ColonyViewDto } from '../features/colonies/dto/view/colony.view.dto';
+import { PaginatorViewModel } from '../../common/dto/view-models/paginator.view.model';
+import { CommandBus } from '@nestjs/cqrs';
+import { User } from '../../account/features/users/domain/user';
+import { PaginatorInputType } from '../../common/dto/input-models/paginator.input.type';
+import { CurrentUserModel } from '../../account/decorators/current-user-model.param.decorator';
+import { PaginatorParam } from '../../common/decorators/paginator-param.decorator';
 
 @Controller('colony')
-export class ColonyController {
+export class ColonyController
+  implements
+    BaseControllerInterface<CreateColonyDto, UpdateColonyDto, ColonyViewDto>
+{
+  constructor(private readonly commandBus: CommandBus) {}
   @Post()
-  create(@Body() createColonyDto: CreateColonyDto) {
-    return;
+  async create(
+    @Body() createColonyDto: CreateColonyDto,
+  ): Promise<ColonyViewDto> {
+    return new ColonyViewDto();
   }
 
   @Get()
-  findAll() {
+  async findAll(
+    @CurrentUserModel() user: User,
+    @PaginatorParam() paginatorParams: PaginatorInputType,
+  ): Promise<PaginatorViewModel<ColonyViewDto>> {
     return;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<ColonyViewDto> {
     return;
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateApiaryDto: UpdateColonyDto) {
+  async update(
+    @Param('id') id: string,
+    @Body() updateApiaryDto: UpdateColonyDto,
+  ) {
     return;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
     return;
   }
 }
