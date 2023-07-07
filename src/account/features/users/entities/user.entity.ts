b3f82de339b30376c6329730692a7500adac8ba2
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-07 23:54:43 +0300

    add method toDomain in UserEntity

--- src/account/features/users/entities/user.entity.ts
+++ src/account/features/users/entities/user.entity.ts
@@ -7,6 +7,7 @@ import {
 } from 'typeorm';
 import { DeviceSessionsEntity } from './device-sessions.entity';
 import { PasswordRecoveryInformationEntity } from './password-recovery-information.entity';
+import { User } from '../domain/user';
 
 @Entity('users')
 export class UserEntity {
@@ -40,4 +41,44 @@ export class UserEntity {
   passwordRecoveryInformation: PasswordRecoveryInformationEntity;
   @OneToMany(() => DeviceSessionsEntity, (ds) => ds.user)
   deviceSessions: DeviceSessionsEntity[];
+
+  toDomain() {
+    console.log('castToUserModel');
+    const user = new User();
+    user.id = String(this.id);
+    user.accountData = {
+      login: this.login,
+      email: this.email,
+      passwordHash: this.passwordHash,
+      passwordSalt: this.passwordSalt,
+      createdAt: +this.createdAt,
+    };
+    user.banInfo = {
+      isBanned: this.isBanned,
+      banDate: +this.banDate || null,
+      banReason: this.banReason,
+      sa: 'superAdmin',
+    };
+    user.emailConfirmation = {
+      isConfirmed: this.isConfirmed,
+      confirmationCode: this.confirmationCode,
+      expirationDate: +this.expirationDate || null,
+      dateSendingConfirmEmail: +this.dateSendingConfirmEmail || null,
+    };
+    user.passwordRecoveryInformation = {
+      recoveryCode: this.passwordRecoveryInformation?.recoveryCode,
+      expirationDate: +this.passwordRecoveryInformation?.expirationDate || null,
+    };
+    if (this.deviceSessions && Array.isArray(this.deviceSessions)) {
+      user.deviceSessions = this.deviceSessions.map((d) => ({
+        deviceId: d.deviceId,
+        ip: d.ip,
+        title: d.title,
+        lastActiveDate: +d.lastActiveDate,
+        expiresDate: +d.expiresDate,
+      }));
+    } else user.deviceSessions = [];
+
+    return user;
+  }
 }
