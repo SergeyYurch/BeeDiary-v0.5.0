e8d0da387fc00a1bed9fb140c8b16a8ccb82b089
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-08 00:00:12 +0300

    refactor colony.view.dto.ts

--- src/apiary/features/colonies/dto/view/colony.view.dto.ts
+++ src/apiary/features/colonies/dto/view/colony.view.dto.ts
@@ -3,7 +3,7 @@ import { QueenViewDto } from '../../../queens/dto/view/queen.view.dto';
 export class ColonyViewDto {
   number: number;
   hiveType: string;
-  nestsFrameType: string; // тип гнездовой рамки
+  nestFrameType: string; // тип гнездовой рамки
   queen: QueenViewDto;
   condition: number;
   note: string;
