e742fcccd4db7fec0b6af9b7c1258249c406e341
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-08 11:11:20 +0300

    added to project frame.entity.ts

---
+++ src/apiary/entities/frame.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from '../../account/features/users/entities/user.entity';
+import { Frame } from '../domain/frame';
+
+@Entity('frames')
+export class FrameEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  createdAt: Date;
+  @Column()
+  type: string;
+  @Column()
+  width: number;
+  @Column()
+  height: number;
+  @Column()
+  cellsNumber: number;
+  @ManyToOne(() => UserEntity)
+  beekeeper: UserEntity;
+  @Column()
+  beekeeperId: number;
+
+  toDomain(): Frame {
+    const frame = new Frame();
+    frame.id = this.id.toString();
+    frame.createdAt = this.createdAt;
+    frame.type = this.type;
+    frame.width = this.width;
+    frame.height = this.height;
+    frame.cellsNumber = this.cellsNumber;
+    frame.beekeeper = this.beekeeper.toDomain();
+    return frame;
+  }
+}
