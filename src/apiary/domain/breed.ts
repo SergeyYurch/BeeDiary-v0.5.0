fd5583c43e8e0ae4224aae35611e80b9fdbe35cc
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-08 09:25:20 +0300

    refactor breed domain object - extend baseDomain

--- src/apiary/domain/breed.ts
+++ src/apiary/domain/breed.ts
@@ -1,11 +1,13 @@
 import { CreateBreedDto } from '../features/breeds/dto/input/create-breed.dto';
 import { User } from '../../account/features/users/domain/user';
+import { BaseDomain } from './base-domain.class';
 
-export class Breed {
-  id: string;
+export class Breed extends BaseDomain {
   title: string;
   beekeeper: User;
-
+  constructor() {
+    super();
+  }
   static create(inputDto: CreateBreedDto, user: User): Breed {
     const domainModel = new Breed();
     domainModel.title = inputDto.title;
