e325126371790779fe45f720179049e607e4d8b1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-08 09:53:06 +0300

    added input dto  - frame-update.input.dto.ts-

---
+++ src/apiary/features/frames/dto/input/frame-update.input.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsPositive, IsString } from 'class-validator';
+
+export class FrameUpdateInputDto {
+  @ApiProperty()
+  @IsString()
+  type: string;
+
+  @ApiProperty()
+  @IsPositive()
+  @IsInt()
+  width: number;
+
+  @ApiProperty()
+  @IsPositive()
+  @IsInt()
+  height: number;
+
+  @ApiProperty()
+  @IsPositive()
+  @IsInt()
+  cellsNumber: number;
+}
