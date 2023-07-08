d884efc5a3089592627ad18e32a5d981411c43f1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-08 09:51:54 +0300

    refactored frame domain obj - implemented extension from the base domain class

--- src/apiary/domain/frame.ts
+++ src/apiary/domain/frame.ts
@@ -1,7 +1,11 @@
-export class Frame {
-  id: number;
+import { BaseDomain } from '../../common/decorators/base-domain.class';
+
+export class Frame extends BaseDomain {
   type: string;
   width: number;
   height: number;
   cellsNumber: number;
+  constructor() {
+    super();
+  }
 }
