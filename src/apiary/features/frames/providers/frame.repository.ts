31f74e96fdf66088c2eff97326c1e7ba1b369267
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-08 11:19:31 +0300

    added to project frame.repository.ts

---
+++ src/apiary/features/frames/providers/frame.repository.ts
@@ -0,0 +1,31 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { FrameEntity } from '../../../entities/frame.entity';
+import { Frame } from '../../../domain/frame';
+import { FrameQueryRepository } from './frame.query.repository';
+
+export class FrameRepository {
+  constructor(
+    private readonly queryRepository: FrameQueryRepository,
+    @InjectRepository(FrameEntity)
+    private readonly frameEntityRepository: Repository<FrameEntity>,
+  ) {}
+  async save(frame: Frame) {
+    let frameEntity = new FrameEntity();
+    if (frame.id)
+      frameEntity = await this.queryRepository.findEntityById(+frame.id);
+    frameEntity.createdAt = frame.createdAt;
+    frameEntity.type = frame.type;
+    frameEntity.width = frame.width;
+    frameEntity.height = frame.height;
+    frameEntity.cellsNumber = frame.cellsNumber;
+    frameEntity.beekeeperId = +frame.beekeeper.id;
+    await this.frameEntityRepository.save(frameEntity);
+    return frameEntity.id.toString();
+  }
+
+  async delete(id: string) {
+    const res: DeleteResult = await this.frameEntityRepository.delete(+id);
+    return res.affected > 0;
+  }
+}
