cd4011ccb4f419d5100c1771b5eaa4dc7a290477
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-09 15:11:10 +0300

    refactoring frame.ts using function isPositiveInt()

--- src/apiary/domain/frame.ts
+++ src/apiary/domain/frame.ts
@@ -2,6 +2,7 @@ import { BaseDomain } from '../../common/decorators/base-domain.class';
 import { FrameCreateDto } from '../features/frames/dto/input/frame-create.dto';
 import { FrameUpdateDto } from '../features/frames/dto/input/frame-update.dto';
 import { User } from '../../account/features/users/domain/user';
+import { isPositiveInt } from '../../common/helpers/helpers';
 
 export class Frame extends BaseDomain {
   type: string;
@@ -16,36 +17,11 @@ export class Frame extends BaseDomain {
     const frame = new Frame();
     frame.beekeeper = user;
     frame.type = inputDto.type ?? 'unknown';
-
-    if (
-      inputDto.width &&
-      Number.isInteger(inputDto.width) &&
-      inputDto.width > 0
-    ) {
-      frame.width = inputDto.width;
-    } else {
-      frame.width = 0;
-    }
-
-    if (
-      inputDto.height &&
-      Number.isInteger(inputDto.height) &&
-      inputDto.height > 0
-    ) {
-      frame.height = inputDto.height;
-    } else {
-      frame.height = 0;
-    }
-
-    if (
-      inputDto.numberOfCells &&
-      Number.isInteger(inputDto.numberOfCells) &&
-      inputDto.numberOfCells > 0
-    ) {
-      frame.numberOfCells = inputDto.numberOfCells;
-    } else {
-      frame.numberOfCells = 0;
-    }
+    frame.width = isPositiveInt(inputDto.width) ? inputDto.width : 0;
+    frame.height = isPositiveInt(inputDto.height) ? inputDto.height : 0;
+    frame.numberOfCells = isPositiveInt(inputDto.numberOfCells)
+      ? inputDto.numberOfCells
+      : 0;
     return frame;
   }
 
