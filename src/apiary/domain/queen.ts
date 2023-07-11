b843209dc13a44d80273d3dc155d8cedd0e2df5e
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-11 15:41:40 +0300

    added queen domain model

--- src/apiary/domain/queen.ts
+++ src/apiary/domain/queen.ts
@@ -1,14 +1,41 @@
 import { BaseDomain } from '../../common/decorators/base-domain.class';
 import { Breed } from './breed';
 import { Grafting } from './grafting';
+import { User } from '../../account/features/users/domain/user';
+import { QueenCreateDto } from '../features/queens/dto/input/queen.create.dto';
+import { QueenUpdateDto } from '../features/queens/dto/input/queen.update.dto';
 
 export class Queen extends BaseDomain {
   constructor() {
     super();
   }
   breed: Breed;
-  flybyDate: Date;
-  note: string;
-  condition: number;
-  grafting: Grafting;
+  flybyMonth: number | null;
+  flybyYear: number | null;
+  note: string | null;
+  condition: number | null;
+  grafting: Grafting | null;
+  static create(
+    inputDto: QueenCreateDto,
+    user: User,
+    breed: Breed,
+    grafting: Grafting,
+  ) {
+    const queen = new Queen();
+    queen.breed = breed;
+    queen.flybyMonth = inputDto.flybyMonth;
+    queen.flybyYear = inputDto.flybyYear;
+    queen.note = inputDto.note;
+    queen.condition = inputDto.condition;
+    queen.grafting = grafting;
+    return queen;
+  }
+  update(updateDto: QueenUpdateDto, breed: Breed, grafting: Grafting | null) {
+    this.breed = breed;
+    this.flybyMonth = updateDto.flybyMonth;
+    this.flybyYear = updateDto.flybyYear;
+    this.note = updateDto.note;
+    this.condition = updateDto.condition;
+    this.grafting = grafting;
+  }
 }
