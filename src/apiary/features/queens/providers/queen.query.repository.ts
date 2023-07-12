605dc98ab9e341efcf4d76c1850bd47bcbd988c1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-12 13:46:26 +0300

    added queen repositories

---
+++ src/apiary/features/queens/providers/queen.query.repository.ts
@@ -0,0 +1,73 @@
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { PaginatorInputType } from '../../../../common/dto/input-models/paginator.input.type';
+import { pagesCount } from '../../../../common/helpers/helpers';
+import { PaginatorViewModel } from '../../../../common/dto/view-models/paginator.view.model';
+import { QueenService } from './queen.service';
+import { BaseQueryRepository } from '../../frames/providers/base.query.repository';
+import { QueenEntity } from '../../../entities/queen.entity';
+import { QueenViewModel } from '../dto/view/queen.view.model';
+import { Queen } from '../../../domain/queen';
+
+export class QueenQueryRepository extends BaseQueryRepository {
+  constructor(
+    @InjectRepository(QueenEntity)
+    private readonly entityRepository: Repository<QueenEntity>,
+    @InjectDataSource() private readonly dataSource: DataSource,
+    private readonly service: QueenService,
+  ) {
+    super();
+  }
+
+  async findEntityById(id: number): Promise<QueenEntity> {
+    return this.entityRepository.findOne({
+      relations: { beekeeper: true, breed: true },
+      where: { id },
+    });
+  }
+
+  async getAllDomainModels(
+    paginatorParams: PaginatorInputType,
+    userId: string,
+  ): Promise<PaginatorViewModel<QueenViewModel>> {
+    const { pageSize, pageNumber } = paginatorParams;
+    const [entities, totalCount] = await this.entityRepository.findAndCount({
+      relations: { beekeeper: true, breed: true },
+      where: { beekeeperId: +userId },
+      skip: pageSize * (pageNumber - 1),
+      take: pageSize,
+    });
+    const domainModels = entities.map((e) => e.toDomain());
+    const items = domainModels.map((dm) => this.service.getViewModel(dm));
+    return {
+      pagesCount: pagesCount(totalCount, pageSize),
+      page: pageNumber,
+      pageSize,
+      totalCount,
+      items,
+    };
+  }
+
+  async getDomainModel(id: string): Promise<Queen> {
+    const entity = await this.findEntityById(+id);
+    if (!entity) return null;
+    return entity.toDomain();
+  }
+
+  async doesIdExist(id: number): Promise<boolean> {
+    try {
+      const queryString = `SELECT EXISTS (SELECT * FROM queens WHERE id=${id});`;
+      const result = await this.dataSource.query(queryString);
+      return result[0].exists;
+    } catch (e) {
+      console.log(e);
+      throw new Error('Database query error');
+    }
+  }
+
+  async isOwner(userId: string, id: string): Promise<boolean> {
+    const queryString = `SELECT q."beekeeperId"=${+userId} as "isOwner" FROM queens q WHERE q.id=${+id};`;
+    const result = await this.dataSource.query(queryString);
+    return result[0].isOwner;
+  }
+}
---
+++ src/apiary/features/queens/providers/queen.repository.ts
@@ -0,0 +1,34 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { QueenQueryRepository } from './queen.query.repository';
+import { HiveEntity } from '../../../entities/hive.entity';
+import { QueenEntity } from '../../../entities/queen.entity';
+import { Queen } from '../../../domain/queen';
+
+export class QueenRepository {
+  constructor(
+    private readonly queryRepository: QueenQueryRepository,
+    @InjectRepository(HiveEntity)
+    private readonly entityRepository: Repository<QueenEntity>,
+  ) {}
+  async save(domainModel: Queen) {
+    let entity = new QueenEntity();
+    if (domainModel.id)
+      entity = await this.queryRepository.findEntityById(+domainModel.id);
+    entity.createdAt = domainModel.createdAt;
+    entity.flybyMonth = domainModel.flybyMonth;
+    entity.flybyYear = domainModel.flybyYear;
+    entity.condition = domainModel.condition;
+    entity.note = domainModel.note;
+    entity.beekeeperId = +domainModel.beekeeper.id;
+    entity.breedId = +domainModel.breed?.id || null;
+    if (domainModel.breed === null) entity.breed = null;
+    await this.entityRepository.save(entity);
+    return entity.id.toString();
+  }
+
+  async delete(id: string) {
+    const res: DeleteResult = await this.entityRepository.delete(+id);
+    return res.affected > 0;
+  }
+}
