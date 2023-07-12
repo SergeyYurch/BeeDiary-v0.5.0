73388170e1a5d1679b16339e32470cfc1bffdc75
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-12 14:08:06 +0300

    deleted files

--- src/apiary/features/queens/dto/input/create-queen.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateQueenDto {
-  bread: string;
-  yearOfBirth: number;
-  monthOfFlyby: number;
-  condition: number;
-  note: string;
-}
--- src/apiary/features/queens/dto/input/update-queen.dto.ts
+++
@@ -1,7 +0,0 @@
-export class UpdateQueenDto {
-  bread: string;
-  yearOfBirth: number;
-  monthOfFlyby: number;
-  condition: number;
-  note: number;
-}
