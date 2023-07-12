552c548006f2c96242a4ae96a8f85f87dd1e1b21
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-12 14:09:02 +0300

    refactor queen.entity.ts

--- src/apiary/entities/queen.entity.ts
+++ src/apiary/entities/queen.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { UserEntity } from '../../account/features/users/entities/user.entity';
 import { BreedEntity } from './breed.entity';
+import { Queen } from '../domain/queen';
 
 @Entity('queens')
 export class QueenEntity {
@@ -24,7 +25,18 @@ export class QueenEntity {
   beekeeper: UserEntity;
   @Column({ nullable: true })
   beekeeperId: number;
-
+  toDomain(): Queen {
+    const queen = new Queen();
+    queen.id = this.id.toString();
+    queen.createdAt = this.createdAt;
+    queen.breed = this.breed.toDomain();
+    queen.flybyMonth = this.flybyMonth;
+    queen.flybyYear = this.flybyYear;
+    queen.condition = this.condition;
+    queen.note = this.note;
+    queen.beekeeper = this.beekeeper.toDomain();
+    return queen;
+  }
   // @ManyToOne(() => GraftingEntity, { nullable: true })
   //  grafting: GraftingEntity| null;
   // @Column{ nullable: true })
