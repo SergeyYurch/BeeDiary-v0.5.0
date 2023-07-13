d50b5372f7c666a46bb1d5cc2597471808c4b7e4
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-07-13 23:52:26 +0300

    refactored colony Domain Object - added create and update methods

--- src/apiary/domain/colony.ts
+++ src/apiary/domain/colony.ts
@@ -3,16 +3,47 @@ import { Hive } from './hive';
 import { Frame } from './frame';
 import { Queen } from './queen';
 import { DangerStatusEnum } from '../types/danger-status.enum';
+import { ColonyCreateDto } from '../features/colonies/dto/input/colony.create.dto';
+import { UpdateColonyDto } from '../../../dist/apiary/features/colonies/dto/input/update-colony.dto';
 
 export class Colony extends BaseDomain {
   number: number;
   hive: Hive;
-  nestsFrameType: Frame;
+  nestFrameType: Frame;
   queen: Queen;
   condition: number;
   note: string;
   status: DangerStatusEnum;
   constructor() {
     super();
+  }
+  static create(
+    createDto: ColonyCreateDto,
+    hive: Hive,
+    frame: Frame,
+    queen: Queen,
+  ) {
+    const colony = new Colony();
+    colony.number = createDto.number;
+    colony.hive = hive;
+    colony.nestFrameType = frame;
+    colony.queen = queen;
+    colony.condition = createDto.condition;
+    colony.note = createDto.note;
+    colony.status = createDto.status;
+  }
+  update(
+    updateDto: UpdateColonyDto,
+    hive: Hive | null = null,
+    frame: Frame | null = null,
+    queen: Queen | null = null,
+  ) {
+    this.number = updateDto.number;
+    this.hive = hive;
+    this.nestFrameType = frame;
+    this.queen = queen;
+    this.condition = updateDto.condition;
+    this.note = updateDto.note;
+    this.status = updateDto.status;
   }
 }
